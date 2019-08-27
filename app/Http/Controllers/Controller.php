106378bbbf2643d09d4562036d359891fd576089
shihongda <shihongda@zsfucai.cn>
2019-08-27 16:20:54 +0800

    Initial commit

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,14 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function json($code, $message = '', $data = [], $meta = [])
+    {
+        return response()->json([
+            'code' => $code,
+            'msg' => $message,
+            'data' => $data,
+            'meta' => $meta,
+        ]);
+    }
 }
